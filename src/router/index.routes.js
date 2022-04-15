d0149e265f360d0bcf6bd378786523c94e3a3e97
MacMohammed <macmohammed@gmail.com>
2022-04-15 11:14:12 -0300

    Outra tentativa

--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -16,8 +16,9 @@ export const navigateTo = url => {
 
 export const router = async () => {
     const routes = [
-        { path: "/", view: Login },
-        { path: "/login", view: Login },
+        // { path: "/", view: Login },
+        // { path: "/login", view: Login },
+        { path: "/", view: GerarArquivo },
         { path: "/gerar-arquivo", view: GerarArquivo },
         { path: "/exportar-arquivo", view: ExportarArquivo },
         { path: "/importar-arquivo", view: ImportarArquivo },

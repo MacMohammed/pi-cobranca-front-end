940da49659d223ce0eb37b0d7836e392cd97c569
MacMohammed <macmohammed@gmail.com>
2022-04-16 08:15:45 -0300

    Outro teste

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '.dist')));
 
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));

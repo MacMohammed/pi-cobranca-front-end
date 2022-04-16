73bed2e83b078ee8c6d5e58e00b81d566d07773d
MacMohammed <macmohammed@gmail.com>
2022-04-16 08:49:21 -0300

    Mais um teste

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const path = require('path');
+// const express = require('express');
+// const path = require('path');
 
-const port = process.env.PORT || 3000;
-const app = express();
-
-app.use(express.static(path.join(__dirname, '.dist')));
-
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`server started port: ${port}`);
-  }
-});
-
-
-
-// const express = require("express");
-// const { resolve } = require("path");
-
+// const port = process.env.PORT || 3000;
 // const app = express();
 
-// app.use("/", express.static(resolve(__dirname, "./dist")));
+// app.use(express.static(path.join(__dirname, '.dist')));
 
-// app.listen(process.env.PORT || 3000, (err) => {
-//   if (err) {
-//     return console.log(err);
-//   }
-//   console.log("Funcionando corretamente!");
+// app.get('*', (_req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist/index.html'));
 // });
+
+// app.listen(port, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(`server started port: ${port}`);
+//   }
+// });
+
+
+
+const express = require("express");
+const { resolve } = require("path");
+
+const app = express();
+
+app.use("/", express.static(resolve(__dirname, ".dist")));
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Funcionando corretamente!");
+});
